62c9afacc93f127f026a208f0b18d7ce28cafb37
apo1967 <apo1967@gmx.de>
2018-02-23 19:55:45 +0100

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dru-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'dru';
+}
