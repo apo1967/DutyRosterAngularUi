c42dd53c729a97da44f6744fcfc9280b0972108d
apo1967 <apo1967@gmx.de>
2018-02-24 14:49:12 +0100

    Added UIKIT, see https://getuikit.com/ Added ng2-file-upload, see https://github.com/valor-software/ng2-file-upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+
+declare var UIkit: any;
+
+// the route to the uploader
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
   selector: 'dru-root',
@@ -7,4 +13,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dru';
+  public uploader:FileUploader = new FileUploader({url: URL});
+  public hasBaseDropZoneOver:boolean = false;
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public showAlert(): void {
+    UIkit.modal.alert('UIkit is working!');
+  }
 }
